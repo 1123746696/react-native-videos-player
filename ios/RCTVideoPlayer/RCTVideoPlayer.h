b1642c86a06e89cc06e1cdd38175a336d8f7ea53
jiangwolong2016 <wolong.jiang@b-and-qchian.com>
2016-11-14 11:33:44 +0800

    add base libary

---
+++ ios/RCTVideoPlayer/RCTVideoPlayer.h
@@ -0,0 +1,13 @@
+//
+//  RCTVideoPlayer.h
+//  RCTVideoPlayer
+//
+//  Created by user on 2016/11/14.
+//  Copyright © 2016年 simple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTVideoPlayer : NSObject
+
+@end
---
+++ ios/RCTVideoPlayer/RCTVideoPlayer.m
@@ -0,0 +1,13 @@
+//
+//  RCTVideoPlayer.m
+//  RCTVideoPlayer
+//
+//  Created by user on 2016/11/14.
+//  Copyright © 2016年 simple. All rights reserved.
+//
+
+#import "RCTVideoPlayer.h"
+
+@implementation RCTVideoPlayer
+
+@end
